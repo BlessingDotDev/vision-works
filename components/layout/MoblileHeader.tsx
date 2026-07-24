a17ef0ca63061490a0a0918df634fa397cddd214
BlessingDotDev <temboblessing05@gmail.com>
2026-07-24 11:26:02 +0200

    channge the color of mobileheader text and icons

--- components/layout/MoblileHeader.tsx
+++ components/layout/MoblileHeader.tsx
@@ -57,8 +57,8 @@ function MobileHeader() {
       {Links.map((link) => (
         <Link href={link.route} key={link.id} className="flex flex-col justify-center items-center gap-2  ">
        
-          <link.icon className="w-6 text-gray-100" strokeWidth={2}/>
-          <p className="text-xs text-gray-100 tracking-wide">{link.title}</p>
+          <link.icon className="w-6 text-gray-300" strokeWidth={2}/>
+          <p className="text-xs text-gray-300 tracking-wide">{link.title}</p>
         
         </Link>
       ))}
